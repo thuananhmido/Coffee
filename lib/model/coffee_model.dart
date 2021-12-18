17757e128f3c454d33008880ddba1550ee05a61f
DESKTOP-5GVBFVQ <thuananhmido@gmail.com>
2021-12-18 22:18:57 +0700

    18/12

--- lib/model/coffee_model.dart
+++ lib/model/coffee_model.dart
@@ -1,14 +1,17 @@
 import 'package:flutter/material.dart';
 
 class Coffee {
-
-  String name, image,description;
+  String name, image, description;
   int price;
   int quantity;
+  String idUser;
 
-  Coffee({ this.description,this.image, this.name, this.price, this.quantity});
-
+  Coffee({
+    this.description,
+    this.image,
+    this.name,
+    this.price,
+    this.quantity,
+    this.idUser,
+  });
 }
-
-
-
