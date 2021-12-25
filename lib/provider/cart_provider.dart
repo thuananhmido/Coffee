efb734f3c71ebc55a0c1b1ace989ab340be91066
DESKTOP-5GVBFVQ <thuananhmido@gmail.com>
2021-12-25 21:29:57 +0700

    25/12

---
+++ lib/provider/cart_provider.dart
@@ -0,0 +1,66 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:CoffeeAppUI/model/coffee_model.dart';
+import 'package:flutter/material.dart';
+
+class CartProvider extends ChangeNotifier {
+  List<Coffee> cartList = [];
+  Coffee cartModle;
+  Future<void> getCartList() async {
+    List<Coffee> newCartList = [];
+    QuerySnapshot querySnapshot = await FirebaseFirestore.instance
+        .collection('data')
+        .where('idUser', isEqualTo: currentUser())
+        .get();
+    querySnapshot.docs.forEach(
+      (element) {
+        cartModle = Coffee(
+          name: element.data()['name'],
+          image: element.data()['image'],
+          price: element.data()['price'],
+          quantity: element.data()['quantity'],
+        );
+        newCartList.add(cartModle);
+      },
+    );
+
+    cartList = newCartList;
+    notifyListeners();
+  }
+
+  get throwCFList {
+    return cartList;
+  }
+
+  int totalprice() {
+    int total = 0;
+    cartList.forEach((element) {
+      total += element.price * element.quantity;
+    });
+    return total;
+  }
+
+  int deleteIndex;
+  void getDeleteIndex(int index) {
+    deleteIndex = index;
+  }
+
+// Future<void> deleteStudent(DocumentSnapshot doc) async {
+//    db.collection("students").document(doc.documentID).delete();
+//    clearForm();
+// }
+  Future<void> delete(namedelete) async {
+    cartList.removeAt(deleteIndex);
+    notifyListeners();
+    FirebaseFirestore.instance.collection('data').doc(namedelete).delete();
+  }
+}
+
+//////////get user///////////////
+final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
+currentUser() {
+  final User user = _firebaseAuth.currentUser;
+  final uid = user.uid.toString();
+  return uid;
+}
